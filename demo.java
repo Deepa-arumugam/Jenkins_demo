804ddd661a1e0569c5c5a2c71d5d492a9d20810f
Deepa-arumugam <79894439+Deepa-arumugam@users.noreply.github.com>
2021-03-22 15:23:47 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,5 +3,7 @@ package jenkinsPractice;
 public class demo {
     public static void main(String[] args) {
         System.out.println("First commit");
+        System.out.println("Second commit");
+
     }
 }
