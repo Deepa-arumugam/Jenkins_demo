07283949e401671a076be0eebc2d1131aace71e8
Deepa-arumugam <79894439+Deepa-arumugam@users.noreply.github.com>
2021-03-22 12:02:25 +0530

    Update DemoJenkin.java

--- DemoJenkin.java
+++ DemoJenkin.java
@@ -1,6 +1,8 @@
 class DemoJenkin{
 public static void main(String[] args){
-  System.out.println("Hello world..");
+  System.out.println("Hello world...");
   System.out.println("First push...");
+   System.out.println("Second push...");
+
    }
 }
