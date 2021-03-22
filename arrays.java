ffd30b8c77551ffbe1e8e4cfc2e1b1a6fd0e6cd4
Deepa-arumugam <79894439+Deepa-arumugam@users.noreply.github.com>
2021-03-22 14:01:31 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,23 @@
+
+public class arrays {
+
+    public static void sort(int[] a){}
+    
+    public static void main(String[] args) {
+        int [] numbers={2,5,1,4,3};
+        for(int x: numbers){
+           // x+=1;
+            System.out.print(x);
+            System.out.print(",");
+        }
+        System.out.print("\n");
+
+        String [] names={"saranya","deepa","suprathi","adhvaithan","aadhiran"};
+        for(String n: names){
+            
+            System.out.print(n);
+            System.out.print("\n");
+        }
+        
+    }   
+}
